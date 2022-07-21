e163358cb6503e030470a144f67e185e4d268510
Kjetil S <kjetilskotheim@gmail.com>
2022-07-21 16:00:39 +0200

    txt

--- challenge-174/kjetillll/perl/ch-1.pl
+++ challenge-174/kjetillll/perl/ch-1.pl
@@ -13,3 +13,12 @@ while( $want > 0 ) {
     $n++;
 
 }
+
+
+__END__
+
+Outputs 0 1 2 3 4 5 6 7 8 9 89 135 175 518 598 1306 1676 2427 2646798
+in about 4-6 seconds which is the first 19 disarium numbers also found at:
+https://oeis.org/A032799
+The next and last known such number is 12157692622039623539
+which would take too long to find. At least with this program.
