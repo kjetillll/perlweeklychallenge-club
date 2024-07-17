1bb77081b73ab9e5f216efcca07e6533091c4635
KjetilS <kjetilskotheim@gmail.com>
2024-07-17 18:05:57 +0200

    bugfix

--- challenge-278/kjetillll/perl/ch-2.pl
+++ challenge-278/kjetillll/perl/ch-2.pl
@@ -2,13 +2,13 @@ use strict; use warnings;
 
 sub f {
     my( $str, $char ) = @_;
-    $str =~ s{ .* $char }{ join '', sort $& =~ /./g }sxer
+    $str =~ s{ .*? $char }{ join '', sort $& =~ /./g }sxer
 }
 
 
 #----------testing
 use Test::More tests=>3;
-ok f(@{$$_{input}}), $$_{output} for
+ok f(@{$$_{input}}) eq $$_{output} for
     { input=> ["challenge", "e"],   output=> "acehllnge" },
     { input=> ["programming", "a"], output=> "agoprrmming" },
     { input=> ["champion", "b"],    output=> "champion" };
